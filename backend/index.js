778ef0292b8fc8504b140bca6ad768ca46703030
TheBinaryZeroOne <dishantchawla123@gmail.com>
2024-11-07 17:28:05 +0530

    updating cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 db();
 
 // middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
